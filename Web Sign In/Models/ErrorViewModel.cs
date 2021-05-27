b06f822174f1fe4a240a1c8e9735b0168e6c344f
Jonathan Moore <Jdcmoore@aol.com>
2021-05-26 22:38:38 -0500

    Add project files.

---
+++ Web Sign In/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Web_Sign_In.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
